3a65531f7eeef62e9c4cd66bbbbafce09699f78e
Stanislav <stas-chubar@yandex.ru>
2023-02-13 06:44:58 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyUniversityTimetable',
+    version='1.0.0',
+    packages=['PyUniTimeTable'],
+    url='https://github.com/schubarkrsk/PyUniversityTimetable',
+    license='GNU GPL V3',
+    author=['S. Chubar', 'A. Semenova'],
+    author_email=['schubar.krsk@icloud.com', 'asemenova@sfu-kras.ru'],
+    description='Simple timetable manager for University'
+)
